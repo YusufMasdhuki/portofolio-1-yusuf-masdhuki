a43b26aab259a25227c84265b39a8625d1b6af8e
YusufMasdhuki <yusufmasduki19@gmail.com>
2025-08-28 06:13:17 +0700

    revisi faq

--- src/constants/faq-data.ts
+++ src/constants/faq-data.ts
@@ -15,24 +15,24 @@ export const faqData: FaqType[] = [
     id: 'q2',
     question: 'Do you work on freelance or remote projects?',
     answer:
-      'Absolutely! I’m open to **freelance and remote collaborations** worldwide. Share your project details, and we can discuss how to bring your ideas to life efficiently and on time.',
+      'Absolutely! I’m open to freelance and remote collaborations worldwide. Share your project details, and we can discuss how to bring your ideas to life efficiently and on time.',
   },
   {
     id: 'q3',
     question: 'Can you convert Figma or Sketch designs into code?',
     answer:
-      'Yes! I can take your **Figma or Sketch designs** and turn them into **pixel-perfect, responsive, and high-performance websites or applications** with clean, maintainable code.',
+      'Yes! I can take your Figma or Sketch designs and turn them into pixel-perfect, responsive, and high-performance websites or applications  with clean, maintainable code.',
   },
   {
     id: 'q4',
     question: 'Do you collaborate with backend developers or teams?',
     answer:
-      'Definitely. I enjoy working in **collaborative environments**, integrating front-end solutions seamlessly with backend APIs or systems to deliver smooth and reliable products.',
+      'Definitely. I enjoy working in collaborative environments, integrating front-end solutions seamlessly with backend APIs or systems to deliver smooth and reliable products.',
   },
   {
     id: 'q5',
     question: 'Are you available for full-time roles?',
     answer:
-      'Yes, I’m open to **full-time opportunities** where I can contribute to building innovative products, grow with a passionate team, and deliver meaningful solutions to users.',
+      'Yes, I’m open to full-time opportunities where I can contribute to building innovative products, grow with a passionate team, and deliver meaningful solutions to users.',
   },
 ];
